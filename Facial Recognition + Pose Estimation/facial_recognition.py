dc98033b038dbbb4c335b7a03ac00fda1006f093
shikhardube03 <shikhardube2003@gmail.com>
2021-11-10 17:30:07 -0600

    Co-authored-by: Codaero <Codaero@users.noreply.github.com>

--- Facial Recognition + Pose Estimation/facial_recognition.py
+++ Facial Recognition + Pose Estimation/facial_recognition.py
@@ -1,5 +1,6 @@
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2.cv2 as cv2
+import face_recognition
 
 cap = cv2.VideoCapture(0)
 detector = FaceDetector()
@@ -8,14 +9,14 @@ while True:
     success, img = cap.read()
     img, bboxs = detector.findFaces(img)
     height, width, channels = img.shape
-    print(height, width, channels)
+    #print(height, width, channels)
 
     if bboxs:
         # bboxInfo - "id","bbox","score","center"
         center = bboxs[0]["center"]
-        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+        # cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
 
-        print(bboxs[0]["bbox"])
+        # print(bboxs[0]["bbox"])
         # bbox - "y-min, x-min, height, width"
         bbox_list = bboxs[0]["bbox"]
 
@@ -23,6 +24,7 @@ while True:
         y_min = int((abs(bbox_list[0]) + bbox_list[0]) / 2)
 
         cropped_image = img[x_min:bbox_list[1] + bbox_list[3], y_min:bbox_list[0] + bbox_list[2]]
+        
         cv2.imshow("cropped", cropped_image)
 
     cv2.imshow("Image", img)
