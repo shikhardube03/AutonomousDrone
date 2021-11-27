19066806e66d1ac334a5b3f4662005b28084699a
shikhardube03 <shikhardube2003@gmail.com>
2021-11-26 22:44:26 -0600

    Co-authored-by: Codaero <Codaero@users.noreply.github.com>

--- Facial Recognition + Pose Estimation/test_one_image.py
+++ Facial Recognition + Pose Estimation/test_one_image.py
@@ -1,16 +1,17 @@
 import face_recognition
 from PIL import Image
+import os
+import sys
 
 
-
-known_image = face_recognition.load_image_file("Facial Recognition + Pose Estimation/Faces/Biden.jpeg")
-unknown_image = face_recognition.load_image_file("Facial Recognition + Pose Estimation/Faces/biden_test.jpg")
+known_image = face_recognition.load_image_file("/Facial Recognition + Pose Estimation/Faces/Biden.jpeg")
+unknown_image = face_recognition.load_image_file("/Facial Recognition + Pose Estimation/Faces/BidenTest.jpg")
 print(known_image)
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 print(biden_encoding)
-# unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-# results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
 
-# print(results)
+print(results)
