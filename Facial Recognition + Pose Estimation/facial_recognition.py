474e842665be7ca65857eb8c990a55a4b596686f
shikhardube03 <shikhardube2003@gmail.com>
2021-11-26 22:46:08 -0600

    Completed Face Encodings

--- Facial Recognition + Pose Estimation/facial_recognition.py
+++ Facial Recognition + Pose Estimation/facial_recognition.py
@@ -1,9 +1,15 @@
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2.cv2 as cv2
 import face_recognition
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
 detector = FaceDetector()
+
+known_image = face_recognition.load_image_file("/Users/shikhar/Desktop/Group52-FA21/Facial Recognition + Pose Estimation/Faces/Shikhar2.jpg")
+known_encoding  = face_recognition.face_encodings(known_image)[0]
+#print(known_image)
+#print(known_encoding)
 
 while True:
     success, img = cap.read()
@@ -24,6 +30,11 @@ while True:
         y_min = int((abs(bbox_list[0]) + bbox_list[0]) / 2)
 
         cropped_image = img[x_min:bbox_list[1] + bbox_list[3], y_min:bbox_list[0] + bbox_list[2]]
+        cropped_encoding = face_recognition.face_encodings(cropped_image)
+        if(len(cropped_encoding) > 0):
+            cropped_encoding = cropped_encoding[0]
+            results = face_recognition.compare_faces([known_encoding], cropped_encoding)
+            print(results)
         
         cv2.imshow("cropped", cropped_image)
 
